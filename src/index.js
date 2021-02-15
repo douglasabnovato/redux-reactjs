2ebae75cf55cb593bd1594ca0f572a4a818df44d
Douglas A B Novato <douglasabnovato@gmail.com>
2021-02-15 11:31:29 -0300

    segundo demo redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
+import store from './app/store';
 import { Provider } from 'react-redux';
-import { Store } from './store';
 
 ReactDOM.render(
-  <Provider store={Store}>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
-); 
+);
