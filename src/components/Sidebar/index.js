0c54c86e3240c03078d07233985fc23b7d5b0f54
Douglas A B Novato <douglasabnovato@gmail.com>
2021-02-17 11:09:47 -0300

    compartilhamento de estado com connect

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,55 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
 
-export default class Sidebar extends Component {
-	state = {
-		modules: [
-			{ 
-				id: 1,
-				title: 'Iniciando com react',
-				lessons:[
-					{
-						id: 1,
-						title: 'Primeira Aula'
-					},
-					{
-						id: 2,
-						title: 'Segunda Aula'
-					}
-				]
-			},
-			{ 
-				id: 2,
-				title: 'Aprendendo Redux',
-				lessons:[
-					{
-						id: 3,
-						title: 'Terceira Aula'
-					},
-					{
-						id: 4,
-						title: 'Quarta Aula'
-					}
-				]
-			}
-		]
-	};
-	render(){
-		const { modules } = this.state;
-		return (
-			<div>
-				<aside>
-					{ modules.map(module => (
-						<div key={ module.id }>
-							<strong>{ module.title }</strong>
-							<ul>
-								{ module.lessons.map(lesson => (
-									<li key={ lesson.id }>{ lesson.title }</li>
-								)) }
-							</ul>
-						</div>
-					))}
-				</aside>
+const Sidebar = ({ modules }) => (
+	<aside>
+		{ modules.map(module => (
+			<div key={ module.id }>
+				<strong>{ module.title }</strong>
+				<ul>
+					{ module.lessons.map(lesson => (
+						<li key={ lesson.id }>{ lesson.title }</li>
+					)) }
+				</ul>
 			</div>
-		)
-	}
-}
+		))}
+	</aside>
+);
+//compartilhamento de estado entre elementos
+export default connect(state => ({ modules: state }))(Sidebar);
