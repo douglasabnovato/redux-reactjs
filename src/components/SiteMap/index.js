ee72ea4706c48031e3312ef9a84c4c7c7006f78a
Douglas A B Novato <douglasabnovato@gmail.com>
2021-02-24 17:38:05 -0300

    component sitemap - redes sociais

--- src/components/SiteMap/index.js
+++ src/components/SiteMap/index.js
@@ -366,8 +366,97 @@ export default function SiteMap() {
                         </div>
                     </div>{/*end-institucional*/} 
                 </div>
-                <div className='footer-default-row'>{/*row-2*/}
-
+                <div className="footer-default-row">{/*row-2*/}
+                    <div>{/*televendas*/}
+                        <div className="title">
+                            Televendas
+                        </div>
+                        <div className="phone">
+                            <a href="https://www.pontofrio.com.br/Site/PopUpMensagem.aspx?XmlTitulo=TituloPopupHelpTelevendas&amp;XmlConteudo=ConteudoPopupHelpTelevendas&amp;KeepThis=true&amp;TB_iframe=true&amp;height=380&amp;width=610?icid=111559">
+                                3004-6688
+                            </a>
+                        </div>
+                        <div>
+                            <span>
+                                "Para comprar ou tirar dúvidas sobre produtos e preços, é só ligar."
+                                <br/>
+                                "De segunda à sexta-feira 08:00 às 23:00."
+                                <br/>
+                                "Sábado 08:00 às 21:00."
+                                <br/>
+                                "Domingo 09:00 às 20:00."
+                            </span>
+                        </div>
+                    </div>{/*end-televendas*/}
+                    <div class="footer-default--attendance">{/*atendimento*/}
+                        <div class="title">
+                            Atendimento
+                        </div>
+                        <div class="attendance">
+                            <div>
+                                <div class="phone">
+                                    <a href="tel:4003-8388">4003-8388</a>
+                                </div>
+                                <div>
+                                    <span>Para clientes de Internet e Televendas</span>
+                                </div>
+                                <div class="phone">
+                                    <a href="tel:0800-200-8388">0800-200-8388</a>
+                                </div>
+                                <div>
+                                    <span>Para RJ E Região</span>
+                                </div>
+                            </div>
+                            <div class="images--or">
+                                <img src="/_next/static/footer.66d3ceb5.png"/>
+                            </div>
+                            <div>
+                                <div class="phone">
+                                    <a href="tel:4002-3388">4002-3388</a>
+                                </div>
+                                <div>
+                                    <span>
+                                        Clientes das Lojas Físicas <br/>
+                                        CRC Pontofrio<br/>
+                                        -<br/>
+                                        Central de Relacionamento com Cliente <br/>
+                                        Para falar conosco, <a href="tel:4002-3388">clique aqui</a>.
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>{/*end-atendimento*/}
+                    <div class="pontofrioFooterDefault--social">{/*Redes Sociais*/}
+                        <div class="title">
+                            Redes Sociais
+                        </div>
+                        <div>
+                            Acompanhe todas as ofertas e novidades do site do Pinguim.
+                        </div>
+                        <div class="social--images">
+                            <a href="https://www.facebook.com/pontofrio" class="images--facebook">
+                                <img src="/_next/static/footer.66d3ceb5.png"/>
+                            </a>
+                            <a href="https://twitter.com/pontofrio" class="images--twitter">
+                                <img src="/_next/static/footer.66d3ceb5.png"/>
+                            </a>
+                            <a href="https://www.instagram.com/pontofrio/" class="images--instagram">
+                                <img src="/_next/static/footer.66d3ceb5.png"/>
+                            </a>
+                            <a class="images--googlePlus">
+                                <img src="/_next/static/footer.66d3ceb5.png"/>
+                            </a>
+                            <a href="http://www.youtube.com/pontofriocom" class="images--youtube">
+                                <img src="/_next/static/footer.66d3ceb5.png"/>
+                            </a>
+                            <a href="http://pinterest.com/amordepinguim" class="images--pinterest">
+                                <img src="/_next/static/footer.66d3ceb5.png"/>
+                            </a>
+                            <a href="https://eglu.pontofrio.com.br/" class="images--wordpress">
+                                <img src="/_next/static/footer.66d3ceb5.png"/>
+                            </a>
+                        </div>
+                    </div>
                 </div>
                 <div className='footer-default-row'>{/*row-3*/}
 
