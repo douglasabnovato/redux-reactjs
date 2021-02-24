d50f04601aef0ec9ba1b690fa44219d50e4c6b07
Douglas A B Novato <douglasabnovato@gmail.com>
2021-02-24 10:21:26 -0300

    header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'; 
+import './global.css';
+
 import { Provider } from 'react-redux';  
 import { PersistGate } from 'redux-persist/integration/react';  
 
 import {store, persistor} from './store'; 
 
+import Header from './components/Header';
 import DevList from './components/DevList';  
 import GitHooks from './components/GitHooks';
 import Location from './components/Location';
@@ -13,6 +16,7 @@ export default class App extends Component {
 	render(){
 		return ( 
 			<Provider store={store}> 
+				<Header />
 				<PersistGate loading={null} persistor={persistor}>
 					<DevList />	
 				</PersistGate> 
---
+++ src/components/Header/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { FaRegStar } from "react-icons/fa"; 
+import LogoNvoip from '../../assets/logo-nvoip-220x51-trans.png';
+
+import './styles.css';
+
+export default function Header(){ 
+
+  return ( 
+    <section className="header-home">  
+        <div>
+            <div className="header-logo">  
+                <a href="https://www.nvoip.com.br/"><img src={LogoNvoip}/></a> 
+            </div>
+            <div className="header-links">
+                <div>
+                    <a href="https://www.nvoip.com.br/">API</a>
+                    <a href="https://www.nvoip.com.br/">Número Nacional</a>
+                    <a href="https://www.nvoip.com.br/">Número Virtual</a>
+                </div>
+            </div>
+        </div>
+    </section>  
+  );
+}
